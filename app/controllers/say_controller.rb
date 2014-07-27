f7ab7a4257a254ff779a6d7265c35109695e6c76
Renato Patron <renato@patrondesigns.com>
2014-07-27 13:42:31 -0500

    Minor changes

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,8 +1,16 @@
 class SayController < ApplicationController
   def hello
   	@time = Time.now
+    # @files = Dir.glob('*')
+  end
+
+  def filenames #adding filenames method
+    @files = Dir.glob('*')
+    @foo = "bar"
+    # return @files
   end
 
   def goodbye
+
   end
 end
